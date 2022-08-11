28804d5b11f6007ea4370fe33458682fa8210ba9
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-11 16:08:16 -0300

    feat: Configurada service com as funções getAll e getById do model product.

---
+++ services/productsService.js
@@ -0,0 +1,9 @@
+const productsModel = require('../models/productsModel');
+
+const getAll = async () => productsModel.getAll();
+const getById = async (id) => productsModel.getById(id);
+
+module.exports = {
+  getAll,
+  getById,
+};
