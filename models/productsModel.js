e87b569a764641d787cccc2e66be1c8e31f13192
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-11 15:53:26 -0300

    feat: Criado product model para solicitar um produto por id ao banco de dados.

--- models/productsModel.js
+++ models/productsModel.js
@@ -6,6 +6,13 @@ const getAll = async () => {
   return products;
 };
 
+const getById = async (id) => {
+  const [product] = await connection
+    .query('SELECT * FROM StoreManager.products WHERE id = ?;', [id]);
+  return product[0];
+};
+
 module.exports = {
   getAll,
+  getById,
 };
