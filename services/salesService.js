0aef8633d57bd326c056c1ada3c67667e8e3f556
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-12 13:25:34 -0300

    Funções para pesquisa de vendas conectadas à camada service.

--- services/salesService.js
+++ services/salesService.js
@@ -1,7 +1,13 @@
 const salesModel = require('../models/salesModel');
 
+const getAll = async () => salesModel.getAll();
+
+const getById = async (id) => salesModel.getById(id);
+
 const add = async (sales) => salesModel.add(sales);
 
 module.exports = {
+  getAll,
+  getById,
   add,
 };
