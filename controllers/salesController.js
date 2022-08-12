a896f0549018335124389dec07ae0b3c6aa6ef55
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-12 13:30:20 -0300

    feat: Implementadas requisições e respostas para pesquisa de vendas na camada controller.

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,6 +1,17 @@
 const salesService = require('../services/salesService');
 const STATUS = require('../utilities/httpStatus');
 
+const getAll = async (_req, res) => {
+  const sales = await salesService.getAll();
+  return res.status(STATUS.OK).json(sales);
+};
+
+const getById = async (req, res) => {
+  const { params: { id } } = req;
+  const sale = await salesService.getById(id);
+  return res.status(STATUS.OK).json(sale);
+};
+
 const add = async (req, res) => {
   const { body: sales } = req;
   const addedSales = await salesService.add(sales);
@@ -8,5 +19,7 @@ const add = async (req, res) => {
 };
 
 module.exports = {
+  getAll,
+  getById,
   add,
 };
