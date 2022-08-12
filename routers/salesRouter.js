a78295608dab30b3b33497096e5b40ac72dde514
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-12 13:38:39 -0300

    feat: Criadas rotas para pesquisa de vendas.

--- routers/salesRouter.js
+++ routers/salesRouter.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const salesController = require('../controllers/salesController');
 const validateSale = require('../middlewares/validateSale');
+const validateSaleId = require('../middlewares/validateSaleId');
 
 const salesRouter = express.Router();
+
+salesRouter.get('/', salesController.getAll);
+
+salesRouter.get('/:id', validateSaleId, salesController.getById);
 
 salesRouter.post('/', validateSale, salesController.add);
 
