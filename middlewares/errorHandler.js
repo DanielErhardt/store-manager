efa4bb3825e2d56ea84c7498b3b7c7318917b814
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-12 00:03:30 -0300

    fix: Adicionado async no middleware errorHandler.

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,4 @@
-const errorHandler = (error, _req, res, _next) => {
+const errorHandler = async (error, _req, res, _next) => {
   const { status, message } = error;
   res.status(status).json({ message });
 };
