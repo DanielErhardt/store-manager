2d5217f191d3067c9918130c2434c2cdaa92d056
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-16 00:09:53 -0300

    refactor: salesService agora faz as validações de consulta ao banco de dados.

--- services/salesService.js
+++ services/salesService.js
@@ -1,14 +1,37 @@
 const salesModel = require('../models/salesModel');
+const error = require('../utilities/expressError');
 
 const getAll = async () => salesModel.getAll();
 
-const getById = async (id) => salesModel.getById(id);
+const getById = async (saleId) => {
+  const saleResult = await salesModel.saleExists(saleId);
+  if (!saleResult) throw error.saleNotFound;
+  return salesModel.getById(saleId);
+};
 
-const add = async (sales) => salesModel.add(sales);
+const add = async (saleProducts) => {
+  const productsExist = await salesModel.saleProductsExist(saleProducts);
+  if (!productsExist) throw error.productNotFound;
+  return salesModel.add(saleProducts);
+};
 
-const edit = async (sale) => salesModel.edit(sale);
+const edit = async (sale) => {
+  const { saleId, products } = sale;
 
-const remove = async (id) => salesModel.remove(id);
+  const saleExists = await salesModel.saleExists(saleId);
+  if (!saleExists) throw error.saleNotFound;
+  
+  const productsExist = await salesModel.saleProductsExist(products);
+  if (!productsExist) throw error.productNotFound;
+
+  return salesModel.edit(sale);
+};
+
+const remove = async (saleId) => {
+  const saleExists = await salesModel.saleExists(saleId);
+  if (!saleExists) throw error.saleNotFound;
+  return salesModel.remove(saleId);
+};
 
 module.exports = {
   getAll,
