541966159b93687d9612dd1801c445cac2bd2604
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-16 10:39:11 -0300

    refactor/errorHandler: Adicionado retorno condicional para erro genérico.

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,8 @@
 const errorHandler = async (error, _req, res, _next) => {
   const { status, message } = error;
-  res.status(status).json({ message });
+  return status
+    ? res.status(status).json({ message })
+    : res.status(500).json({ message: 'Something went wrong' });
 };
 
 module.exports = errorHandler;
