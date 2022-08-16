b9bf24aa49796870ac8de2573cadfa4e4eca346c
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-16 10:29:39 -0300

    refactor/productsService: Implementadas validações de banco de dados.

--- services/productsService.js
+++ services/productsService.js
@@ -1,11 +1,35 @@
+const error = require('../utilities/expressError');
+
 const productsModel = require('../models/productsModel');
 
 const getAll = async () => productsModel.getAll();
-const getById = async (id) => productsModel.getById(id);
+
+const getById = async (productId) => {
+  const productExists = await productsModel.productExists(productId);
+  if (!productExists) throw error.productNotFound;
+  return productsModel.getById(productId);
+};
+
 const add = async (product) => productsModel.add(product);
-const edit = async (product) => productsModel.edit(product);
-const remove = async (id) => productsModel.remove(id);
-const getByName = async (name) => productsModel.getByName(name);
+
+const edit = async (product) => {
+  const { id } = product;
+  const productExists = await productsModel.productExists(id);
+  if (!productExists) throw error.productNotFound;
+  return productsModel.edit(product);
+};
+
+const remove = async (productId) => {
+  const productExists = await productsModel.productExists(productId);
+  if (!productExists) throw error.productNotFound;
+  return productsModel.remove(productId);
+};
+
+const getByName = async (productName) => {
+  const products = await productsModel.getByName(productName);
+  if (!products.length) throw error.productNotFound;
+  return products;
+};
 
 module.exports = {
   getAll,
