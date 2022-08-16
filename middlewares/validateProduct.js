8f4b40d5c0f8c6554002ae27105957626b0e744a
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-16 00:28:16 -0300

    refactor/validateProduct: Validações agora retornam objeto do utilitário expressError.

--- middlewares/validateProduct.js
+++ middlewares/validateProduct.js
@@ -1,20 +1,12 @@
-const STATUS = require('../utilities/httpStatus');
-const Error = require('../classes/CustomError');
+const error = require('../utilities/expressError');
 
-const validateProduct = async (req, res, next) => {
+const validateProduct = async (req, _res, next) => {
   const { body: { name } } = req;
 
-  if (!name) {
-    next(new Error(STATUS.BAD_REQUEST, '"name" is required'));
-    return;
-  }
+  if (!name) return next(error.badRequest('"name" is required'));
 
   if (name.length < 5) {
-    next(new Error(
-      STATUS.UNPROCESSABLE_ENTITY,
-      '"name" length must be at least 5 characters long',
-    ));
-    return;
+    return next(error.unprocessableEntity('"name" length must be at least 5 characters long'));
   }
 
   next();
