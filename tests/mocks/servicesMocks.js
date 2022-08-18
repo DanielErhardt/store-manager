6e50c3c1b476e135bc6462a90c06a9c228d0951e
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-08-18 11:44:11 -0300

    feat: Implementados mocks das funções do arquivo salesService.

--- tests/mocks/servicesMocks.js
+++ tests/mocks/servicesMocks.js
@@ -7,16 +7,46 @@ const servicesMocks = {
 
   getProductById: (id) => servicesMocks.getAllProducts().find((product) => product.id === id),
 
-  getProductsByName: (name) => servicesMocks.getAllProducts
+  getProductsByName: (name) => servicesMocks.getAllProducts()
     .filter((product) => product.name.toLowerCase().includes(name.toLowerCase())),
   
-  addProduct: (name) => { id: 1, name },
+  addProduct: (name) => ({ id: 1, name }),
 
-  editProduct: ({ id, name }) => { id, name },
+  editProduct: (editedProduct) => editedProduct,
 
-  getAllSales: () => [],
+  getAllSales: () => [
+  {
+    saleId: 1,
+    date: '2022-08-18T14:11:50.000Z',
+    productId: 1,
+    quantity: 5
+  },
+  {
+    saleId: 1,
+    date: '2022-08-18T14:11:50.000Z',
+    productId: 2,
+    quantity: 10
+  },
+  {
+    saleId: 2,
+    date: '2022-08-18T14:11:50.000Z',
+    productId: 3,
+    quantity: 15
+  }
+],
 
-  getSaleById: (id) => servicesMocks.getAllSales.find((sale) => sale.id === id),
+  getSaleById: (id) => servicesMocks.getAllSales()
+    .filter((sale) => sale.saleId === id)
+    .map(({date, productId, quantity}) => ({
+      date, productId, quantity,
+    })),
+  
+  addSale: (itemsSold) => ({
+    id: 1,
+    itemsSold,
+  }),
+
+  editSale: ({id, itemsUpdated}) => ({id, itemsUpdated}),
 };
 
 module.exports = servicesMocks;
